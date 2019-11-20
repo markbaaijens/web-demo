399fd7e5489475c3187f65cc78bf523d843e8b17
Mark Baaijens <markbaaijens@Ions-MBP-10.home>
2019-11-20 21:35:58 +0100

    Modified responses, default price value on POST, updated editBook (still not working)

--- api/app.py
+++ api/app.py
@@ -25,27 +25,15 @@ books = [
 
 @app.errorhandler(404)
 def notFoundError(error):
-    message = {
-            'status': 404,
-            'message': 'Not Found: ' + request.url
-    }
-    return make_response(jsonify(message), 404)
+    return make_response(jsonify({'message': 'Not Found: ' + request.url}), 404)
 
 @app.errorhandler(400)
 def invalidBodyError(error):
-    message = {
-            'status': 400,
-            'message': 'Bad request'
-    }
-    return make_response(jsonify(message), 400)
+    return make_response(jsonify({'message': 'Bad request'}), 400)
 
 @app.errorhandler(405)
 def methodNotAllowedError(error):
-    message = {
-            'status': 405,
-            'message': 'Method ' + request.method + ' is not allowed on ' + request.url
-    }
-    return make_response(jsonify(message), 400)
+    return make_response(jsonify({'message': 'Method ' + request.method + ' is not allowed on ' + request.url}), 400)
 
 @app.route('/')
 def root():
@@ -56,7 +44,7 @@ def root():
 @app.route('/books', methods=['get'])
 def getBooks():
     print('get')
-    return jsonify({'books': books})
+    return jsonify({'books': books}), 200
 
 # GET /books/<id>
 # curl -i http://localhost:5000/books/1
@@ -74,18 +62,21 @@ def getBookById(id):
     if len(returnValue) == 0:
         abort(404)
 
-    return jsonify(returnValue)
+    return jsonify(returnValue), 200
 
 # POST /books/<id>
 # curl -i http://localhost:5000/books -H "Content-Type: application/json" -X POST -d '{"isbn": 5, "name":"Name"}' 
 @app.route('/books', methods=['post'])
 def addBook():
-    if not request.json or not 'isbn' in request.json:
+    if not request.json:
         abort(400)
+    if not 'isbn' in request.json:
+        abort(400)
+
     book = {
         'id': books[-1]['id'] + 1,
         'name': request.json['name'],
-        'price': request.json.get('price', ""),
+        'price': request.json.get('price', 0),
         'isbn': request.json.get('isbn', 0),
     }
     books.append(book)
@@ -93,34 +84,31 @@ def addBook():
 
 # PATCH /books
 # curl -i http://localhost:5000/books/3 -H "Content-Type: application/json" -X PATCH -d '{"isbn": 66, "name":"Name"}' @a
-@app.route('/books/<int:id>', methods=['patch'])d
-ef editBook(id):
+@app.route('/books/<int:id>', methods=['patch'])
+def editBook(id):
     book = [book for book in books if book['id'] == id]
     if len(book) == 0:
         abort(404)
 
-    req = request.get_json()
-    print(book)
-    print(req)
+    requestData = request.get_json()
 
-    for key, value in req.items():
+    for key, value in requestData.items():
         print(key + '|' + str(value))
-        #print(book[key])
-        #book[key] = value
 
-        if key =='isbn':  
-            book['isbn'] = key
-        if key == 'name': 
-            book['name'] = key
- #   print(book)
-
- #   book[id] = req
- #   res = make_response(jsonify({"message": "Collection created"}), 201)
- #   return res
-
-    return jsonify({'result': True})
-#    return jsonify({"message": "Collection updated"}), 200)
-
+    updatedBook = {
+        'id': id,
+        'namme': requestData['name'],
+        'isbn': requestData['isbn'],
+        'price': requestData['price']
+    }
+    i = 0
+    for book in books:
+        currentId = book['id']
+        if currentId == id:
+            book[i] = updatedBook
+        i += 1
+        
+    return jsonify(updatedBook), 200
 
 # DELETE /books/<id>
 # curl -i http://localhost:5000/books/3 -X DELETE
@@ -130,7 +118,7 @@ def deleteBook(id):
     if len(book) == 0:
         abort(404)
     books.remove(book[0])
-    return jsonify({'result': True})
+    return "", 200
 
 if __name__ == '__main__':
     app.run(port=5000, debug=True)  # auto-reload
