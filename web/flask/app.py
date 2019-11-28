f854d0a10e5e1dc4be50fd2d48c3351ea45277c0
Mark Baaijens <markbaaijens@Ions-MBP-10.home>
2019-11-28 23:49:10 +0100

    Funtion getApiInfo

--- web/flask/app.py
+++ web/flask/app.py
@@ -3,27 +3,36 @@ import requests
 
 app = Flask(__name__)
 
+# Globals
+apiInfo = []
+
+def getApiInfo():
+    global apiInfo
+    try:
+        # Using eval to convert string to a dictionairy
+        apiInfo = eval(requests.get('http://localhost:5000').content)
+    except:
+        apiInfo = []
+
+    return
+
 # GET /
 @app.route('/', methods=['GET'])
 def index():
+    global apiInfo
 
     try:
         # Using eval to convert string to a dictionairy
         bookList = eval(requests.get('http://localhost:5000/books').content)
     except:
         bookList = []
-
-    try:
-        # Using eval to convert string to a dictionairy
-        apiInfo = eval(requests.get('http://localhost:5000').content)
-    except:
-        apiInfo = []
 
     return render_template('index.html', title = 'Titel', api = apiInfo, books = bookList)
 
 # GET /books
 @app.route('/books', methods=['GET'])
 def books():
+    global apiInfo
 
     try:
         # Using eval to convert string to a dictionairy
@@ -31,13 +40,8 @@ def books():
     except:
         bookList = []
 
-    try:
-        # Using eval to convert string to a dictionairy
-        apiInfo = eval(requests.get('http://localhost:5000').content)
-    except:
-        apiInfo = []
-
     return render_template('books.html', title = 'Titel', api = apiInfo, books = bookList)
 
 if __name__ == '__main__':
+    getApiInfo()
     app.run(port=5001, debug=True)  # auto-reload
