34326621795f1fdefdade82c069f2de46f185dd5
markbaaijens <markbaaijens@Ions-MBP-10.home>
2019-12-14 16:43:05 +0100

    Varchar(30) changed to native type Text

--- api/data/sqlite/schema.sql
+++ api/data/sqlite/schema.sql
@@ -2,7 +2,7 @@ PRAGMA foreign_keys=OFF;
 BEGIN TRANSACTION;
 CREATE TABLE IF NOT EXISTS "Books" (
 	"Id"		Integer NOT NULL DEFAULT 0 PRIMARY KEY AUTOINCREMENT,
-	"Name"		Varchar(30) NOT NULL DEFAULT '',
+	"Name"		Text NOT NULL DEFAULT '' CHECK (length(Name) <= 30),
 	"ISBN"		Integer NOT NULL DEFAULT 0 UNIQUE,
 	"Price"		Numeric NOT NULL DEFAULT 0,
 	"Obsolete" 	Boolean NOT NULL DEFAULT 'False',
