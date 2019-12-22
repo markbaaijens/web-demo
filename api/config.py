8397a9226c0bdeadca73e9d20f252c85ea9ad767
markbaaijens <mark.baaijens@gmail.com>
2019-12-22 23:37:10 +0100

    Formatting

--- api/config.py
+++ api/config.py
@@ -2,8 +2,9 @@ import os
 
 class Config(object):
     SQLITE_FILE_NAME = "data/data.db"
-    
+
     MYSQL_HOST = "localhost"
     MYSQL_USER = "debian-sys-maint"
     MYSQL_PASSWORD = "OX2VZ4XPu8tVWHYx"
     MYSQL_DATABASE = "data"
+    
